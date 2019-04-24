5f4ced47bcf87831471a4840cc81cf5c22c88d37
hongch <hongch@ufotosoft.com>
2019-04-24 20:15:20 +0800

    **[数据库]修改数据库账号密码

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -4,8 +4,8 @@ return array(
 	'DB_TYPE'               =>  'mysql',     // 数据库类型
 	'DB_HOST'               =>  'localhost', // 服务器地址
 	'DB_NAME'               =>  'efucms',       // 数据库名
-	'DB_USER'               =>  'efucms',      // 用户名
-	'DB_PWD'                =>  'kangjia123',      // 密码
+	'DB_USER'               =>  'root',      // 用户名
+	'DB_PWD'                =>  'hzdz20190424',      // 密码
 	'DB_PORT'               =>  '3306',      // 端口
 	'DB_PREFIX'             =>  'vv_',    	 // 数据库表前缀
 );
