8a224282db8950e63d8106d08ece2ff31f3c5586
hongch <hongch@ufotosoft.com>
2019-06-15 13:37:45 +0800

    t

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 	遇到问题请联系售后，不要随意改动程序源代码，一经改动不再享受售后保障服务。
 	您购买的是程序使用权，版权归开发者所有。未经同意不得转售或者修改后再次销售
 	使用本程序则视为接受本协议
-	
+
 ================================================================================
 */
 unset($_GET['m']);
@@ -23,7 +23,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('PHP 版本必须大于等于5
 define('DIR_SECURE_CONTENT', 'powered by http://www.efucms.com');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG', true);
+define('APP_DEBUG', false);
 
 if(!APP_DEBUG){
 	ini_set('display_errors', false);
